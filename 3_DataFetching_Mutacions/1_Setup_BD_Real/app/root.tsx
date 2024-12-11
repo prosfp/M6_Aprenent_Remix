d0b30db012e2b585edd423bab540f131c660e775
PABLO ROS PROFE <pros7@xtec.cat>
2024-12-11 18:50:07 +0100

    updates

---
+++ 3_DataFetching_Mutacions/1_Setup_BD_Real/app/root.tsx
@@ -0,0 +1,50 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+
+import "./tailwind.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    {
+      rel: "preconnect",
+      href: "https://fonts.gstatic.com",
+      crossOrigin: "anonymous",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swaphttps://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap",
+    },
+  ];
+};
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="min-h-screen bg-gradient-to-r from-indigo-500 to-indigo-900">
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return <Outlet />;
+}
---
+++ 3_DataFetching_Mutacions/1_Setup_BD_Real/app/routes/_marketing.auth.tsx
@@ -0,0 +1,9 @@
+import AuthForm from "../components/auth/AuthForm";
+
+export default function AuthPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <AuthForm />
+    </div>
+  );
+}
